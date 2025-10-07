9086d86c9ed42b3a0663efc497f077833c847df4
Andrew Kent <andrew@braintrustdata.com>
2025-10-07 10:58:35 -0600

    test for genai system message

--- src/main/java/dev/braintrust/instrumentation/anthropic/otel/GenAiAttributes.java
+++ src/main/java/dev/braintrust/instrumentation/anthropic/otel/GenAiAttributes.java
@@ -1,13 +1,7 @@
 package dev.braintrust.instrumentation.anthropic.otel;
 
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
-
-import io.opentelemetry.api.common.AttributeKey;
-
 // copied from GenAiIncubatingAttributes
 final class GenAiAttributes {
-    static final AttributeKey<String> GEN_AI_PROVIDER_NAME = stringKey("gen_ai.provider.name");
-
     static final class GenAiOperationNameIncubatingValues {
         static final String CHAT = "chat";
 
